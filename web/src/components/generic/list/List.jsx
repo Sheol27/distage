95f68de559d2cd0be15ef4cfd033546cc23a07a4
Sheol27 <63117338+Sheol27@users.noreply.github.com>
2024-07-29 22:32:39 +0200

    Fix dark mode colors and scrollbar.

--- web/src/components/generic/list/List.jsx
+++ web/src/components/generic/list/List.jsx
@@ -4,21 +4,17 @@ import ListItem from "./ListItem";
 function List({ items, selectedItemId, onItemSelected }) {
   const getType = (status) => (status === "running" ? "success" : "error");
 
-  return (
-    <div>
-      {items.map((x, i) => (
-        <ListItem
-          key={x.id}
-          index={i}
-          mainText={x.hostname}
-          labelText={x.status}
-          labelType={getType(x.status)}
-          selected={i === selectedItemId}
-          onItemSelected={onItemSelected}
-        />
-      ))}
-    </div>
-  );
+  return items.map((x, i) => (
+    <ListItem
+      key={x.id}
+      index={i}
+      mainText={x.hostname}
+      labelText={x.status}
+      labelType={getType(x.status)}
+      selected={i === selectedItemId}
+      onItemSelected={onItemSelected}
+    />
+  ));
 }
 
 export default List;
